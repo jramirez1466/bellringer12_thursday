5648d57806a62c9bc6f438e5d174afdfdada7441
jramirez1466. <cps43254111>
2013-11-08 14:22:47 -0600

    idk

---
+++ sum_diff_product.rb
@@ -0,0 +1,9 @@
+puts "type a number"
+answer = gets.chomp
+
+puts "type another number"
+answerB = gets.chomp
+
+puts answer.to_i + answerB.to_i
+puts answer.to_i - answerB.to_i
+puts answer.to_i * answerB.to_i
