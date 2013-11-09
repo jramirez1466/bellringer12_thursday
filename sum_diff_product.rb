13aa5f3699fedc9490ee5c82e2561f4154d961c4
Giovanni Delatorre <gdelatorre@chicagotechacademy.org>
2013-11-09 14:57:54 -0600

    class work

--- sum_diff_product.rb
+++ sum_diff_product.rb
@@ -1,25 +1,7 @@
 puts "type a number"
-answer = gets.chomp
+answerA = gets.chomp
 
 puts "type another number"
 answerB = gets.chomp
 
-puts  + answerB.to_i
-puts answer.to_i - answerB.to_i
-puts answer.to_i * answerB.to_i
-
-def method_name (a)
-gets.chomp = a 
-	split the "12" on the empty space 
-
-	two object recived 
-
-	sum = 12 + 2 
-	diff = 12 - 2
-	pro 12*2 
-
-	puts "#{s} = sum, #{d} = diff, ..."
-
-end
-
-method_name(a)
+def add_two(number)
